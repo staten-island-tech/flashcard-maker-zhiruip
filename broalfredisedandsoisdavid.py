def7008e0c29b0200c4c5381b056abcafe5c48fb
zhiruip <ZHiruip@nycstudents.net>
2025-03-27 09:43:26 -0400

    Create broalfredisedandsoisdavid.py

---
+++ broalfredisedandsoisdavid.py
@@ -0,0 +1,23 @@
+import json
+
+role = input("are you a teacher or a student?")
+def flashcards():
+    if role == "teacher":
+        teacher()
+    else:
+        student()
+
+class flashcardvalues:
+    def __init__(self, key, value, image=None):
+        self.key == y
+        self.value == x
+
+
+def teacher():
+    global x, y
+    x = input("input your question")
+    y = input("input your answer")
+    print("stored into database")
+    
+
+    
